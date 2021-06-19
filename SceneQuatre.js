6aeaf6f046a21b52132b9f94197949ff33e81338
TomSachot <74669910+TomSachot@users.noreply.github.com>
2021-06-19 23:42:49 +0200

    Create SceneQuatre.js

---
+++ SceneQuatre.js
@@ -0,0 +1,529 @@
+
+
+class SceneQuatre extends Phaser.Scene{
+    constructor(){
+        super("sceneQuatre");
+        this.pad = null;
+    }
+    init(data){
+    }
+    preload(){
+        // Assets
+        this.load.image('lave_fond', 'assets/lave.png');
+        this.load.image('mur_grotte_fond', 'assets/mur_grotte.png');
+        this.load.image('vapeur_fond', 'assets/vapeur.png');
+
+        this.load.spritesheet('player','assets/blyke.png', { frameWidth: 32, frameHeight: 36 } );
+        this.load.spritesheet('player_invinsible','assets/blyke_invinsible.png', { frameWidth: 32, frameHeight: 36 } );
+        this.load.spritesheet('boules_blyke','assets/boules_blyke.png', {frameWidth: 25, frameHeight: 25});
+        this.load.image('power_up','assets/power_up.png');
+        this.load.image('power_up_vie','assets/power_up_vie.png');
+
+        this.load.spritesheet('boss', 'assets/boss.png', { frameWidth: 56, frameHeight: 88 });
+        this.load.spritesheet('sharingan','assets/sharingan.png', {frameWidth: 42, frameHeight: 42});
+
+        this.load.image('paroi_grande','assets/paroi_grande.png');
+        this.load.image('paroi_petite','assets/paroi_petite.png');
+
+        this.load.image('tiles_4','assets/tiles_solmur_grotte.png');
+        this.load.tilemapTiledJSON('map_4','assets/Grotte2.json');
+
+        this.load.image('gameover','assets/gameover.png');
+
+    }
+    create(){
+        //Affichage des assets
+
+        this.add.image(2000, 240, 'lave_fond').setScrollFactor(0.8);
+        this.add.image(2000, 240, 'mur_grotte_fond').setScrollFactor(0.9);
+        this.add.image(2000, 480, 'vapeur_fond').setScrollFactor(0.6);
+
+        const map_4 = this.make.tilemap({key: 'map_4'});
+        const tileset = map_4.addTilesetImage('tiles_solmur_grotte','tiles_4');
+
+        sol_grotte2 = map_4.createLayer('sol_grotte2', tileset, 0, 0);
+        platforms_grotte2 = map_4.createLayer('platforms_grotte2', tileset, 0, 0);
+        piege_grotte2 = map_4.createLayer('piege_grotte2', tileset, 0, 0);
+
+        sol_grotte2.setCollisionByExclusion(-1, true);
+        platforms_grotte2.setCollisionByExclusion(-1, true);
+        piege_grotte2.setCollisionByExclusion(-1, true);
+
+        player = this.physics.add.sprite(position4playerX, position4playerY, 'player');
+        power_up = this.physics.add.sprite(position4playerX, position4playerY, 'power_up');
+
+        power_up_vie = this.physics.add.sprite(505, 430, 'power_up_vie');
+
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(false);
+
+        //Animations de déplacements du joueur
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'player', frame: 4 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'turn_invinsible',
+            frames: [ { key: 'player_invinsible', frame: 4 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'q',
+            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'q_invinsible',
+            frames: this.anims.generateFrameNumbers('player_invinsible', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'd',
+            frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'd_invinsible',
+            frames: this.anims.generateFrameNumbers('player_invinsible', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        //Attaque : Boules de Blyke
+
+        const boules_blykeObjects = this.physics.add.sprite(position4playerX, positionplayerY, 'boules_blyke');
+        this.boules_blykes = this.physics.add.group({
+            allowGravity: false
+        });
+
+        //Animations des boules de Blyke
+
+        this.anims.create({
+            key: 'boules_blyke_turn',
+            frames: [ { key: 'boules_blykes', frame: 0 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'boules_blyke_left',
+            frames: this.anims.generateFrameNumbers('boules_blyke', { start: 0, end: 1 }),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'boules_blyke_right',
+            frames: this.anims.generateFrameNumbers('boules_blyke', { start: 2, end: 3 }),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'boules_blyke_haut',
+            frames: this.anims.generateFrameNumbers('boules_blyke', { start: 4, end: 5 }),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'boules_blyke_bas',
+            frames: this.anims.generateFrameNumbers('boules_blyke', { start: 6, end: 7 }),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        //Ennemi : Boss Silence
+
+        const bossObjects = map_4.getObjectLayer('boss_grotte2').objects;
+        this.bosss = this.physics.add.group({
+            allowGravity: true
+        });
+
+        for (const boss of bossObjects) {
+            this.bosss.create(boss.x, boss.y, 'boss')
+                .setOrigin(0.5,0.5)
+                .setDepth(1)
+                .setScale(1)
+                .setGravityY(300)
+        }
+
+        //Animations de Silence
+
+        this.anims.create({
+            key: 'boss_turn',
+            frames: [ { key: 'boss', frame: 4 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'boss_left',
+            frames: this.anims.generateFrameNumbers('boss', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'boss_right',
+            frames: this.anims.generateFrameNumbers('boss', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        //Ennemi : Projectile de Silence
+
+        const sharinganObjects = map_4.getObjectLayer('boss_grotte2').objects;
+        this.sharingans = this.physics.add.group({
+            allowGravity: false
+        });
+
+        //Animations des projectiles de Silence
+
+        this.anims.create({
+            key: 'sharingan_turn',
+            frames: [ { key: 'sharingan', frame: 0 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'sharingan_right',
+            frames: this.anims.generateFrameNumbers('sharingan', { start: 0, end: 3 }),
+            frameRate: 20,
+            repeat: -1
+        });
+
+       //Parois
+
+        //paroi_grande = this.physics.add.sprite(1151, 430, 'paroi_grande');
+
+       const paroi_petiteObjects = map_4.getObjectLayer('paroi_sortie_grotte2').objects;
+       this.paroi_petites = this.physics.add.group({
+           allowGravity: false
+       });
+
+        //Clavier
+        keys = this.input.keyboard.addKeys({
+            q: Phaser.Input.Keyboard.KeyCodes.Q,
+            d: Phaser.Input.Keyboard.KeyCodes.D,
+            z: Phaser.Input.Keyboard.KeyCodes.Z,
+            e: Phaser.Input.Keyboard.KeyCodes.E,
+            s: Phaser.Input.Keyboard.KeyCodes.S,
+            space: Phaser.Input.Keyboard.KeyCodes.SPACE,
+            enter: Phaser.Input.Keyboard.KeyCodes.ENTER,
+        });
+
+        this.physics.add.collider(player, sol_grotte2);
+        this.physics.add.collider(player, platforms_grotte2);
+        this.physics.add.collider(player, paroi_grande);
+        this.physics.add.collider(player, piege_grotte2, piegemort, null, this);
+        this.physics.add.collider(power_up, sol_grotte2);
+        this.physics.add.collider(power_up, platforms_grotte2);
+        this.physics.add.collider(player, power_up, collectPowerUp, null, this);
+        this.physics.add.collider(power_up_vie, sol_grotte2);
+        this.physics.add.collider(power_up_vie, platforms_grotte2);
+        this.physics.add.collider(player, power_up_vie, collectPowerUpVie, null, this);
+
+        this.physics.add.collider(sol_grotte2, paroi_grande);
+
+        this.physics.add.collider(this.bosss, sol_grotte2);
+        this.physics.add.collider(this.bosss, platforms_grotte2);
+        this.physics.add.collider(this.sharingans, sol_grotte2);
+
+        //Caméra
+        this.cameras.main.startFollow(player);
+        this.cameras.main.setBounds(0, 0, map_4.widthInPixels, map_4.heightInPixels);
+
+        //Affichage de la position du joueur
+        positionXtext = this.add.text(0, 0, 'PositionX', + player.x, { fontSize: '15px', fill: '#f00'} ).setScrollFactor(0).setDepth(1);
+        positionYtext = this.add.text(0, 20, 'PositionY', + player.y, { fontSize: '15px', fill: '#f00'} ).setScrollFactor(0).setDepth(1);
+    }
+    update(){
+        //Affichage de la position du joueur
+        positionXtext.setText('Position X :' + player.x);
+        positionYtext.setText('Position Y :' + player.y);
+    
+        //Game Over
+        if (gameOver){
+            gameOverText = this.add.image(896/2, 448/2, 'gameover').setScrollFactor(0).setDepth(1);
+            if(keys.enter.isDown){
+                    this.scene.restart();
+                    HP = 6;
+                    gameOver = false;
+                    jet2 = true;
+                    jet = true;
+                    
+            }
+            return;
+        }
+
+       //Déplacements basiques : droite, gauche et saut
+
+       if(keys.q.isDown){
+            player.setVelocityX(-185);
+            player.anims.play('q', true);
+        }
+        else if(keys.d.isDown){
+            player.setVelocityX(185);
+            player.anims.play('d', true);
+        }
+        else{
+            player.setVelocityX(0);
+            player.anims.play('turn', true);
+        }
+
+        if(keys.z.isDown && player.body.blocked.down){
+            player.setVelocityY(-170);
+        }
+
+        if(jetpack == true){
+            if(keys.space.isDown){
+                player.setVelocityY(-110);
+            }
+        
+
+            if(keys.e.isDown && keys.q.isDown && jet2 == true){
+                jet2 = false;
+                boules_blyke = this.physics.add.sprite(player.x, player.y, 'boules_blyke');
+                boules_blyke.setVelocityX(-400);
+                boules_blyke.body.allowGravity = false;
+                boules_blyke.play('boules_blyke_left', true);
+                this.physics.add.collider(boules_blyke, sol_grotte2, disparitionboules, null, this);
+                this.physics.add.collider(boules_blyke, platforms_grotte2, disparitionboules, null, this);
+                this.physics.add.collider(boules_blyke, sharingan, destructsharingan, null, this);
+                this.physics.add.collider(boules_blyke, this.bosss, destructboss, null, this);
+            }
+
+
+            if(keys.e.isDown && keys.d.isDown && jet2 == true){
+                jet2 = false;
+                boules_blyke = this.physics.add.sprite(player.x, player.y, 'boules_blyke');
+                boules_blyke.setVelocityX(400);
+                boules_blyke.body.allowGravity = false;
+                boules_blyke.play('boules_blyke_right', true);
+                this.physics.add.collider(boules_blyke, sol_grotte2, disparitionboules, null, this);
+                this.physics.add.collider(boules_blyke, platforms_grotte2, disparitionboules, null, this);
+                this.physics.add.collider(boules_blyke, sharingan, destructsharingan, null, this);
+                this.physics.add.collider(boules_blyke, this.bosss, destructboss, null, this);
+            }
+
+            if(keys.e.isDown && keys.z.isDown && jet2 == true){
+                jet2 = false;
+                boules_blyke = this.physics.add.sprite(player.x, player.y, 'boules_blyke');
+                boules_blyke.setVelocityY(-400);
+                boules_blyke.body.allowGravity = false;
+                boules_blyke.play('boules_blyke_haut', true);
+                this.physics.add.collider(boules_blyke, sol_grotte2, disparitionboules, null, this);
+                this.physics.add.collider(boules_blyke, platforms_grotte2, disparitionboules, null, this);
+                this.physics.add.collider(boules_blyke, sharingan, destructsharingan, null, this);
+                this.physics.add.collider(boules_blyke, this.bosss, destructboss, null, this);
+            }
+
+            if(keys.e.isDown && keys.s.isDown && jet2 == true){
+                jet2 = false;
+                boules_blyke = this.physics.add.sprite(player.x, player.y, 'boules_blyke');
+                boules_blyke.setVelocityY(400);
+                boules_blyke.body.allowGravity = false;
+                boules_blyke.play('boules_blyke_bas', true);
+                this.physics.add.collider(boules_blyke, sol_grotte2, disparitionboules, null, this);
+                this.physics.add.collider(boules_blyke, platforms_grotte2, disparitionboules, null, this);
+                this.physics.add.collider(boules_blyke, sharingan, destructsharingan, null, this);
+                this.physics.add.collider(boules_blyke, this.bosss, destructboss, null, this);
+            }
+        }
+
+        //Perte des points de vie de Blyke
+
+        if(PV = true){
+            if(HP == 0){
+                gameOver = true;
+            }
+        }
+
+        //Compteur pour invincibilité
+
+        if(invincible == true){
+            compteur-- ;
+            if(compteur == 0){
+                compteur = 150;
+                invincible = false ;
+            }
+        }
+
+        //Déplacements de Silence
+
+        for (const boss of this.bosss.children.entries) {
+
+
+            if (boss.body.blocked.right) {
+                boss.direction = 'LEFT';
+                
+            }
+            
+            else if (boss.body.blocked.left) {
+                boss.direction = 'RIGHT';
+            }
+
+            else{
+                boss.setVelocityX(200);
+            }
+
+            if(boss.direction === 'LEFT'){
+                boss.setVelocityX(-200);
+                boss.anims.play('boss_left', true);
+
+            }
+
+            else if(boss.direction === 'RIGHT'){
+                boss.setVelocityX(200);
+                boss.anims.play('boss_right', true);
+            }
+        }
+
+        //Apparition des parois
+
+        if(player.x >= 1264){
+            paroi_grande = this.physics.add.sprite(1151, 430, 'paroi_grande');
+            paroi_grande.setVelocityY(-60);
+            paroi_grande.setImmovable(true);
+            paroi_grande.body.allowGravity = false;
+            if(paroi_grande.y = 176){
+                paroi_grande.setVelocityY(0);
+                this.physics.add.collider(player, paroi_grande);
+            }
+        }
+
+        if(player.x <= 3074){
+            paroi_petite = this.physics.add.sprite(3074, 368, 'paroi_petite');
+            paroi_petite.setVelocityY(-60);
+            paroi_petite.setImmovable(true);
+            paroi_petite.body.allowGravity = false;
+            if(paroi_petite.y = 368){
+                paroi_petite.setVelocityY(0);
+                this.physics.add.collider(player, paroi_petite);
+            }
+        }
+
+
+        //Tirs de projectile de Silence
+
+        for (const boss of this.bosss.children.entries) {
+            
+            
+            if(player.x >= 1264 && player.x <= 3050 && jet == 1){
+                jet = 0;
+                console.log("sharingan");
+                if(player.x < boss.x){
+                    sharingan = this.physics.add.sprite(boss.x, boss.y, 'sharingan');
+                    sharingan.setVelocityX(-200);
+                    this.physics.moveToObject(sharingan, player, 200);
+                    sharingan.body.allowGravity = false;
+                    sharingan.play('sharingan_right', true);
+                    this.physics.add.collider(sharingan, sol_grotte2, disparitionsharingan, null, this);
+                    this.physics.add.collider(player, sharingan, sharinganmort, null, this);
+                    this.physics.add.collider(sharingan, boules_blyke, destructsharingan, null, this);
+                }
+                if(player.x > boss.x){
+                    sharingan = this.physics.add.sprite(boss.x, boss.y, 'sharingan');
+                    sharingan.setVelocityX(-200);
+                    this.physics.moveToObject(sharingan, player, 200);
+                    sharingan.body.allowGravity = false;
+                    sharingan.play('sharingan_right', true);
+                    this.physics.add.collider(sharingan, sol_grotte2, disparitionsharingan, null, this);
+                    this.physics.add.collider(player, sharingan, sharinganmort, null, this);
+                    this.physics.add.collider(sharingan, boules_blyke, destructsharingan, null, this);
+                }          
+            }
+        }
+    }
+}
+
+    //Power-Up (Jetpack)
+
+    function collectPowerUp(player, power_up){
+
+        power_up.disableBody(true, true);
+        recupPowerUp = true;
+        jetpack = true;
+    }
+
+    //Power-Up (Jetpack)
+
+    function collectPowerUpVie(player, power_up_vie){
+
+        power_up_vie.disableBody(true, true);
+        recupPowerUpVie = true;
+        PV = true;
+    }
+
+    //Piège
+
+    function piegemort(player, piege_ext2){
+
+        this.physics.pause();
+        player.anims.play('turn', true);
+        player.setTint(0x000000);
+        gameOver = true;
+    }
+
+
+    
+
+    function destructsharingan(boules_blyke, sharingan){
+
+        boules_blyke.disableBody(true, true);
+        boules_blyke.body.destroy();
+        sharingan.disableBody(true, true);
+        sharingan.body.destroy();
+        jet2 = true;
+        jet = true;
+
+    }
+
+    //Mort du boss Silence
+
+    function destructboss(boss, boules_blyke){
+
+        boss.disableBody(true, true);
+        boss.destroy();
+        jet2 = true;
+        jet = false;
+    }
+
+    //Sharigan
+
+    function disparitionsharingan(sharingan, sol_grotte2){
+
+        sharingan.disableBody(true, true);
+        sharingan.body.destroy();
+        jet = true;
+
+    }
+
+    function sharinganmort(player, sharingan){
+
+        if(HP >= 1){
+            HP = HP - 1;
+            invincible = true;
+            player.setTint(0xff0000);
+            sharingan.disableBody(true, true);
+            sharingan.body.destroy();
+            jet = true;
+        }
+        if(HP == 0){
+            this.physics.pause();
+            player.anims.play('turn', true);
+            player.setTint(0x000000);
+            sharingan.disableBody(true, true);
+            sharingan.body.destroy();
+            gameOver = true;
+        }
+    
+    }
